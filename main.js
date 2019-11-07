7acd16dc77b5ae8ee3fe44dc527313f53c6d7575
Ricardo Boluda Hernández <riboher@gmail.com>
2019-11-07 19:40:12 +0100

    Book class slot and send telegram msg

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
+const getDay = require('date-fns/getDay');
+
 require('dotenv').config();
 
 (async () => {
-  const browser = await puppeteer.launch({ devtools: true })
+  const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
   await page.goto('https://www.vivagym.es/login', { waitUntil: 'networkidle2' }),
@@ -11,10 +13,10 @@ require('dotenv').config();
   await page.click('.cc-dismiss')
 
   await page.focus('#emailInput')
-  await page.keyboard.type('riboher@gmail.com')
+  await page.keyboard.type(process.env.USER_EMAIL)
 
   await page.focus('#passwordInput')
-  await page.keyboard.type('30066877')
+  await page.keyboard.type(process.env.USER_PASS)
 
   await Promise.all([
     page.click('button[type="submit"]'),
@@ -27,11 +29,40 @@ require('dotenv').config();
   await page.click('a[href="/bookClasses"]')
 
   await page.waitFor('.classespadding')
+  await page.click('.handcursor.pull-right')
+  await page.waitFor(2000)
 
-  await axios.post(`https://api.telegram.org/bot${process.env.TOKEN_ID}/sendMessage`, {
-    chat_id: process.env.CHAT_ID,
-    text: 'Gimnasio reservado'
+  const {
+    classSlotSelector,
+    buttonTargetID,
+    className,
+    classTime,
+    isClassAvailable
+  } = await page.evaluate(() => {
+    const classSlotSelector = '.classespadding .panel-body > .row > div:nth-child(14) > .au-target'
+    const node = document.querySelector(classSlotSelector)
+    const className = node.childNodes[0].textContent
+    const classTime = node.childNodes[1].textContent
+    const bookClassBtn = node.lastChild.getElementsByTagName('button')[0]
+    const isClassAvailable = !bookClassBtn.classList.contains('disabled')
+    const buttonTargetID = bookClassBtn.getAttribute('au-target-id')
+    return {
+      classSlotSelector,
+      buttonTargetID,
+      className,
+      classTime,
+      isClassAvailable
+    }
   })
+
+  if (isClassAvailable) {
+    page.click(`${classSlotSelector} button[au-target-id="${buttonTargetID}"]`)
+    await axios.post(`https://api.telegram.org/bot${process.env.TOKEN_ID}/sendMessage`, {
+      chat_id: process.env.CHAT_ID,
+      text: `Has reservado una clase de ${className} en el horario de ${classTime}`
+    })
+  }
+
 
   await browser.close()
 })()
